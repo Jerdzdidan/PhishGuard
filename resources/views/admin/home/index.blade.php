07d394a2fd2c3b2c0b06fe9325e94c5f45bd3022
Jerdzdidan <jdgonzdayao@gmail.com>
2026-01-27 12:56:57 +0800

    border radius welcome picture

--- resources/views/admin/home/index.blade.php
+++ resources/views/admin/home/index.blade.php
@@ -6,6 +6,6 @@ HOME
 
 @section('body')
 <div class="d-flex justify-content-center align-items-center" style="min-height: 80vh;">
-    <img src="{{ asset('img/home/home.png') }}" alt="Home" class="img-fluid" style="max-width: 100%; height: auto;">
+    <img src="{{ asset('img/home/home.png') }}" alt="Home" class="img-fluid" style="max-width: 100%; height: auto; border-radius: 10px;">
 </div>
 @endsection
